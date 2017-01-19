0c03492631662db4d0896dac6d7fa682318fc745
Jens Jorritsma <jens.jorritsma@rackspace.com>
2017-01-18 19:32:30 -0600

    Initial addition

---
+++ comparison.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+
+'''
+Create a Python library from scratch that will compare two semantic version
+strings. A successful implementation will be able to test two strings for the
+following:
+    Equal (==)
+    Greater than (>)
+    Greater than or equal to (>=)
+    Pessimistic (~>)
+    Less than (<)
+    Less than or equal to (<=)
+Constraints:
+    Only use libraries built into Python itself (ie. os, sys, etc). Nothing
+        from pypi or other external sources.
+    Use Python 2.7 or 3+
+'''
+
+
+def compare_values(x, y):
+    check_valid(x, y)
+    return compare_version(x,y)
+
+
+def compare_version(x, y):
+    if x > y:
+        return 1
+    elif x < y:
+        return -1
+    elif x == y:
+        return 0
+    else:
+        return "wat?"
+
+
+def check_valid(x, y):
+    try:
+        determine_valid(x)
+        determine_valid(y)
+
+
+def determine_valid(x):
+    version_split = x.split(".")
+    if len(version_split) < 2:
+        return False
+    for place in version_split:
+        if (place[:1] == "0") and (len(place) > 1):
+            return False
+    if len(version_split) >= 2:
+        if (version_split[0] == "0") and (version_split[1] == "0"):
+            return False
+
+
+if __name__ == '__main__':
+    test = compare_values("3.2.11", "0")
+    print test
---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='coding-challenge',
+      version='0.1',
+      description='Coding challenge',
+      author='Jens Jorritsma',
+      author_email='jens.jorritsma@rackspace.com',
+      url='https://github.com/jensjorritsma/coding-challenge',
+      packages=['distutils', 'distutils.command'],
+     )
